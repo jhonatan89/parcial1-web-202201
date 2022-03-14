10fa12547d7259f208199b8a2544655862f95bf1
Manuel Sosa <mr.sosa@uniandes.edu.co>
2022-03-14 18:50:42 -0500

    parcial

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,38 @@
-const getUser = async () => {
+const { default: axios } = require("axios");
+const res = require("express/lib/response");
+
+const getUser = async (username) => {
+  let resp;
+
   //TODO user logic
-  return { message: 'To be implemented' };
+  const names = await axios(`https://mauvelous-leopard-5257.twil.io/friends`);
+  const userData = await names.data.friends.map((x)=>x.username);
+
+  let user = userData.some(u => username == u);
+  if (user == true){
+    const servicioPlays = await axios(`https://mauvelous-leopard-5257.twil.io/plays-detail?username=${username}`);
+    const plays =servicioPlays.data.plays;
+    const nplays = plays.length;
+
+    const servicioFiends = await axios(`https://mauvelous-leopard-5257.twil.io/friend-detail?username=${username}`);
+    const friends = servicioFiends.data.friends;
+    const nfriends = friends.length;
+
+    resp = {
+      "username": username,
+      "plays": nplays,
+      "friends": nfriends,
+      "tracks": [
+        plays
+      ],
+      "uri": `/users/${username}`
+    }
+  }
+  else{
+    resp="no existe username";
+  }
+  
+  return { message: resp };
 };
 
 module.exports = { getUser };
